1d51faabd740df75484edea30fe73fa1ef032037
stanislav-az <st.zhdanovich@gmail.com>
2021-03-03 03:23:25 +0700

    add log with time example

--- src/Lib.hs
+++ src/Lib.hs
@@ -4,10 +4,15 @@ module Lib
 
 import qualified Colog as Log
 import Data.Functor.Contravariant (Contravariant(contramap))
+import qualified Data.ByteString as BS
 
 someFunc :: IO ()
-someFunc = Log.usingLoggerT (contramap Log.fmtMessage Log.logTextStdout) example
+someFunc = Log.usingLoggerT (Log.upgradeMessageAction Log.defaultFieldMap $ Log.cmapM Log.fmtRichMessageDefault Log.logTextStdout) example
 
+fmt :: Log.RichMsg m Log.Message -> m BS.ByteString
+fmt = error "not implemented"
+
+-- type WithLog env msg m = (MonadReader env m, HasLog env msg m)
 example :: Log.WithLog env Log.Message m => m ()
 example = do
   Log.logDebug "Starting application..."
