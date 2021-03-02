8b6d755e869ccd0e415a2cfa139abf88ec701dd8
Stanislav Zhdanovich <st.zhdanovich@gmail.com>
2021-03-02 20:48:09 +0700

    add log examples

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,14 @@
 module Lib
-    ( someFunc
-    ) where
+  ( someFunc
+  ) where
+
+import qualified Colog as Log
+import Data.Functor.Contravariant (Contravariant(contramap))
 
 someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+someFunc = Log.usingLoggerT (contramap Log.fmtMessage Log.logTextStdout) example
+
+example :: Log.WithLog env Log.Message m => m ()
+example = do
+  Log.logDebug "Starting application..."
+  Log.logInfo "Finishing application..."
