b2394b41a069c2be572839f828b80899d850e460
stanislav-az <st.zhdanovich@gmail.com>
2021-03-12 00:53:40 +0700

    check local config

--- app/Main.hs
+++ app/Main.hs
@@ -2,6 +2,13 @@ module Main where
 
 import Ext.Logger.Colog (setLineBuffering)
 import Lib (runMigrationsAndServer)
+import qualified System.Directory as FS
 
 main :: IO ()
-main = setLineBuffering >> runMigrationsAndServer
+main = setLineBuffering >> checkLocalConfig >> runMigrationsAndServer
+
+checkLocalConfig :: IO ()
+checkLocalConfig = do
+  let cfgPath = "./config/local.conf"
+  isExist <- FS.doesFileExist cfgPath
+  if isExist then pure () else writeFile cfgPath ""
